7c1c467ced8523a0482fcb8fb4e31f746cbddd2c
darshanalafille <s.darshana@directfn.com>
2020-04-24 18:23:49 +0530

    initial commit 	new file:   package-lock.json 	new file:   package.json 	new file:   server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require('express');
+const axious = require('axios');
+const cors = require('cors');
+const ip = require('ip');
+
+const randomString = require('randomstring');
+const xxx = randomString.generate({
+  length: 7,
+  charset: 'numeric'
+});
+const ipAddress = ip.address();
+// Constants
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+var results = {  
+  serverId : xxx,
+  ip:ipAddress
+};
+
+
+// App
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  //res.send('Hello World : ' + xxx);
+  
+  axious.get('http://localhost:8081').
+    then(resp => {		
+		
+      console.log(resp.data);
+      results.auth = resp.data;
+      axious.get("http://localhost:8082").
+        then(r => {
+          results.items = r.data;
+          res.json(results);
+        });
+      
+    });
+	
+	
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
